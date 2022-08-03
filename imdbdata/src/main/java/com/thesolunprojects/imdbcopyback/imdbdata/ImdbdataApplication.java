dbb40c715671f03d1c882e0facba900a0f4137c9
mmolinari_cit <mmolinari@ciandt.com>
2022-08-03 14:03:01 -0300

    feat: Spring Intializr maven project with java language in version 11 and the dependencies spring web, spring data jpa and ms sql server driver

---
+++ imdbdata/src/main/java/com/thesolunprojects/imdbcopyback/imdbdata/ImdbdataApplication.java
@@ -0,0 +1,13 @@
+package com.thesolunprojects.imdbcopyback.imdbdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbdataApplication.class, args);
+	}
+
+}
---
+++ imdbdata/src/test/java/com/thesolunprojects/imdbcopyback/imdbdata/ImdbdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesolunprojects.imdbcopyback.imdbdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
