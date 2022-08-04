91ae070589094f5355fcc31ba5672428734643b8
TheSolun <mateusferrazf4@gmail.com>
2022-08-04 13:17:50 -0300

    feat: first implementation of core TitleBasicsRepository

--- imdbdata/src/main/java/com/thesolunprojects/imdbcopyback/imdbdata/core/repository/TitleBasicsRepository.java
+++ imdbdata/src/main/java/com/thesolunprojects/imdbcopyback/imdbdata/core/repository/TitleBasicsRepository.java
@@ -1,7 +1,13 @@
 package com.thesolunprojects.imdbcopyback.imdbdata.core.repository;
 
+import java.util.List;
+
+import com.thesolunprojects.imdbcopyback.imdbdata.core.entity.TitleBasicsEntity;
+
 public interface TitleBasicsRepository {
 
-	
+	List<TitleBasicsEntity> findAll();
+
+	List<TitleBasicsEntity> findByIsAdultStartYearAndGenre(boolean isAdult, String startYear, String genre);
 	
 }
